3fbf01d7b0403823684a27725a9c5e12b2c8c3e9
Colin <deniscolin777@yahoo.fr>
2022-09-09 16:30:36 +0200

    090922

---
+++ ProjetEchecDAL/Migrations/20220909085231_090922.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetEchecDAL.Migrations
+{
+    public partial class _090922 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
